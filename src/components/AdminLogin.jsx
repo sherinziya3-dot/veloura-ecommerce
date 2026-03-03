6d9ce0faef1fbf0cc9943b46d878c5412217e411
shafna <Sherinziya3@gmail.com>
2026-03-04 00:56:40 +0530

    Removed backend and fixed SingleProduct

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { setSession } from "../utils/isAuthenticated";
-import adminsData from "../data/admins";
+import adminsData from "../data/admin";
 
 export default function AdminLogin() {
   const navigate = useNavigate();
